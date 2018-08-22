0ca9449433f83b3dfa19180e12f3e6d6142937aa
Warren Strange <warren.strange@gmail.com>
2018-08-22 09:31:40 -0600

    first commit

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Get current dir (so run this script from anywhere)
+export DOTFILES_DIR 
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Make utilities available
+
+EXTRA_PATH=$DOTFILES_DIR/bin:~/.pub-cache/bin
+PATH="$EXTRA_PATH/bin:$PATH"
+
+# Bunch of symlinks
+
+ln -sfv "$DOTFILES_DIR/.zshrc" ~
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+
+# Package managers & packages
+
+cd $HOME
