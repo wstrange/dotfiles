5b6732437139e3db5ff1f3974c1d7c20511064ac
Warren Strange <warren.strange@forgerock.com>
2020-05-01 13:55:52 -0600

    stuff

--- linux.sh
+++ linux.sh
@@ -4,32 +4,19 @@
 apt-get install zsh gcc
 
 
-# Go dep
-curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
-
-
-go get github.com/kubernetes-sigs/kustomize
-
-# skaffold
-curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/v0.13.0/skaffold-linux-amd64 && chmod +x skaffold && sudo mv skaffold /usr/local/bin
-
+# https://ohmyz.sh/#install
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sudo snap install --classic kubectl go
 sudo snap install docker
 
 
-packages="zsh"
-
 # linux brew
  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
 
 brew install skaffold kustomize
 brew install derailed/k9s/k9s
 
-
-# https://ohmyz.sh/#install
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Krew
 # https://krew.sigs.k8s.io/docs/user-guide/setup/install/
@@ -41,25 +28,29 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
   "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
   "$KREW" update
 )
-
-
-# Append
-# export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
-# export PATH="${PATH}:/snap/bin"
-
 kubectl krew install ctx ns tail
 
 
+# Append
+cat >>~/.zshrc <<EOF
 
 alias k=kubectl
+
 export PATH="${PATH}:${HOME}/.krew/bin"
 export PATH="${PATH}:/snap/bin"
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+EOF
 
- git config --global user.email "warren.strange@forgerock.com"
-
+git config --global user.email "warren.strange@forgerock.com"
 git config --global user.name "Warren Strange"
 
 
+sudo snap remove google-cloud-sdk
+# Install the cloud sdk
+curl https://sdk.cloud.google.com | bash
+
+
+
 # I had to use a service account for docker push
+# This sets the permissons on the bucket
 gsutil iam ch serviceAccount:docker-build@forgeops-public.iam.gserviceaccount.com:objectAdmin gs://artifacts.forgeops-public.appspot.com/
